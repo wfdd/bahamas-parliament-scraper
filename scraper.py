7ceaef395c502b7db58af2c75977235ee084f941
wfdd <erdbanfed@gmail.com>
2016-06-13 03:48:49 +0300

    Scrape the secretaries, who are listed separately

--- scraper.py
+++ scraper.py
@@ -9,7 +9,10 @@ import sys
 from lxml.html import document_fromstring as parse_html
 from splinter import Browser
 
-base_url = 'http://www.bahamas.gov.bs/wps/portal/public/?urile=wcm%3apath%3a/MOF_Content/internet/The+Government/Government/The+Government/Legislative/Members+of+Parliament/'
+base_url = 'http://www.bahamas.gov.bs/wps/portal/public/'
+list_urls = '?1dmy&urile=wcm%3apath%3a/MOF_Content/internet/The+Government/Government/The+Government/Legislative/Members+of+Parliament/', \
+            '?1dmy&urile=wcm%3apath%3a/MOF_Content/internet/All+Contacts/Directory+of+Contacts/Listed+by+Parliamentary+Secretaries/'
+
 honorifics = ('The Hon.', 'Dr.', 'MP, ')
 
 
@@ -30,42 +33,45 @@ def extract_birth_date(text):
         return date
 
 
-def scrape_rows(session, rows):
-    for row in rows:
-        profile_link = urljoin(base_url, row.xpath('.//a/@href')[0])
+def scrape_row(session, row):
+    profile_url = urljoin(base_url, row.xpath('.//a/@href')[0])
+    constituency, island, group = (None,) * 3
+    if 'Parliamentary Secretaries' not in row.xpath('string(//title)'):
         constituency, island, group = ((*i.xpath('./text()'), '')[0].strip()
                                        for i in row.xpath('./td[position() > 1]'))
-        name, = row.xpath('.//a/text()')
-        last, first = (i.strip()
-                       for i in ft.reduce(lambda s, r: s.replace(r, ''),
-                                          honorifics, name).split(','))
+    name, = row.xpath('.//a/text()')
+    last, first = (i.strip()
+                   for i in ft.reduce(lambda s, r: s.replace(r, ''),
+                                      honorifics, name).split(','))
 
-        session.visit(profile_link)
-        html = parse_html(session.html)
-        image, = html.xpath('//img[@class = "alignLeft sidePicture"]/@src')
-        image = urljoin(base_url, image)
-        yield (first + ' ' + last,
-               last + ', ' + first,
-               last,
-               first,
-               extract_birth_date(html.xpath('//div[text() = "Biography"]'
-                                             '/following-sibling::p')),
-               image,
-               group.strip('()'),
-               constituency,
-               island,
-               profile_link)
+    session.visit(profile_url)
+    html = parse_html(session.html)
+    image, = html.xpath('//img[@class = "alignLeft sidePicture"]/@src')
+    image = urljoin(base_url, image)
+    return (first + ' ' + last,
+            last + ', ' + first,
+            last,
+            first,
+            extract_birth_date(html.xpath('//div[text() = "Biography"]'
+                                          '/following-sibling::p')),
+            image,
+            group and group.strip('()'),
+            constituency,
+            island,
+            profile_url)
 
 
-def gather_people(session):
-    session.visit(base_url)
-    while True:
-        yield from iter(parse_html(session.html)
-                        .xpath('//table[@class = "detailTable detailTable_full"]/tbody/tr'))
-        next_page = session.find_by_xpath('//a[@title = "Link to next page"]')
-        if not next_page:
-            break
-        next_page.click()
+def collect_rows(session):
+    for list_url in list_urls:
+        session.visit(urljoin(base_url, list_url))
+        while True:
+            yield from iter(parse_html(session.html)
+                            .xpath('//table[@class = "detailTable detailTable_full"]/tbody/tr'))
+
+            next_page = session.find_by_xpath('//a[@title = "Link to next page"]')
+            if not next_page:
+                break
+            next_page.click()
 
 
 def main():
@@ -80,7 +86,9 @@ CREATE TABLE IF NOT EXISTS data
         c.executemany('''\
 INSERT OR REPLACE INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
             (lambda date: ((*p, date)
-                           for p in people))(dt.date.today().isoformat()))
+                           for p in (scrape_row(session, r)
+                                     for r in tuple(collect_rows(session)))
+                           ))(dt.date.today().isoformat()))
 
 if __name__ == '__main__':
     main()
